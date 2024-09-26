afd1a8a916b0cb82ca3871704c46353f9e2657b5
TangentLime <tl.tangentlime@gmail.com>
2024-09-26 00:46:45 -0500

    Changed variable naming conventions and reduced storage size slightly

--- gigemcar_v3.ino
+++ gigemcar_v3.ino
@@ -10,27 +10,30 @@ const int photo3 = 35;
 const int photo4 = 32;
 const int startlever = 25;
 const int servoPin = 33;
-const int motor = 23;
+
+//both motors are on one circuit
+const int motors = 23;
 
 //State variables for things
-int off1 = 0;
-int off2 = 0;
-int off3 = 0;
-int off4 = 0;
-int offTotal = 0;
+bool photo1_isOff = 0;
+bool photo2_isOff = 0;
+bool photo3_isOff = 0;
+bool photo4_isOff = 0;
+
+uint8_t offTotal = 0;
 int status = 0;
 int status2 = 0;
 
-//Photoresistor Sensitivities????
-const int sense1 = 200;
-const int sense2 = 200;
-const int sense3 = 200;
-const int sense4 = 1200;
+//Photoresistor Sensitivities
+const int photo1_sense = 200;
+const int photo2_sense = 200;
+const int photo3_sense = 200;
+const int photo4_sense = 1200;
 
 int timer = 0;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(115200);    //TODO: Find out what this means
   valve.attach(servoPin, 1000, 2000);
 
   //Initialize hardware
@@ -41,7 +44,7 @@ void setup() {
 
   pinMode(startlever, INPUT); // Use internal pull-up resistor
 
-  pinMode(motor, OUTPUT);
+  pinMode(motors, OUTPUT);
   delay(3000);
 }
 
@@ -52,59 +55,63 @@ void loop() {
     Serial.println("starting");
     valve.write(180); //OPEN VALVE
     timer = timer + 100; //RUN TIMER    <- Ask about using arduino-timer https://github.com/contrem/arduino-timer
-    digitalWrite(motor, HIGH);
+    digitalWrite(motors, HIGH);
     lightcheckFunction(); //CHECK PHOTORESISTORS
     delay(100); //10 CYCLES PER SECOND     <- Ask about making faster
   } 
   else {
     valve.write(0);
-    digitalWrite(motor, LOW);
+    digitalWrite(motors, LOW);
     Serial.println("not operating");
     delay(250);
   }
 }
 
 void lightcheckFunction() {
-  int light1 = analogRead(photo1);
-  int light2 = analogRead(photo2);
-  int light3 = analogRead(photo3);
-  int light4 = analogRead(photo4);
+  int photo1_val = analogRead(photo1);
+  int photo2_val = analogRead(photo2);
+  int photo3_val = analogRead(photo3);
+  int photo4_val = analogRead(photo4);
 
-  off1 = (light1 < sense1) ? 1 : 0;
-  off2 = (light2 < sense2) ? 1 : 0;
-  off3 = (light3 < sense3) ? 1 : 0;
-  off4 = (light4 < sense4) ? 1 : 0;
-
-  offTotal = off1 + off2 + off3 + off4;
+  //If the light value is higher than the sensitivity, 
+  //then the value will stay false, otherwise it will flip
+  photo1_isOff = (photo1_val < photo1_sense);
+  photo2_isOff = (photo2_val < photo2_sense);
+  photo3_isOff = (photo3_val < photo3_sense);
+  photo4_isOff = (photo4_val < photo4_sense);
+ 
+  offTotal = photo1_isOff + photo2_isOff + photo3_isOff + photo4_isOff;
 
   Serial.println("RUNNING");
   Serial.print('\t');
-  Serial.print(light1);
+  Serial.print(photo1_val);
   Serial.print('\t');
-  Serial.print(light2);
+  Serial.print(photo2_val);
   Serial.print('\t');
-  Serial.print(light3);
+  Serial.print(photo3_val);
   Serial.print('\t');
-  Serial.print(light4);
+  Serial.print(photo4_val);
   Serial.print('\t');
-  Serial.print(off1);
-  Serial.print(off2);
-  Serial.print(off3);
-  Serial.print(off4);
+  Serial.print(photo1_isOff);
+  Serial.print(photo2_isOff);
+  Serial.print(photo3_isOff);
+  Serial.print(photo4_isOff);
   Serial.print("\t");
   Serial.println(offTotal);
 
-  if (offTotal > 2) {
+  //Checks if any three are on using boolean logic
+  //if ((photo1_isOff && photo2_isOff && photo3_isOff) || (photo1_isOff && photo2_isOff && photo4_isOff) || (photo2_isOff && photo3_isOff && photo4_isOff) || (photo1_isOff && photo3_isOff && photo4_isOff)) {
+    if (offTotal >= 3) {
     Serial.println("STOPPED");
-    digitalWrite(motor, LOW);
+    digitalWrite(motors, LOW);
     Serial.println("final time");
     Serial.println(timer);
 
-    while (true) {
+    while (true) { //why is this only here
       status2 = digitalRead(startlever);
       if (status2==LOW){
         Serial.println("reset");
-        timer =0;
+        timer = 0;
         break;
       }
 
